9b3e6af091f761b30797546102a16a820bd9b0fc
buding983 <66161138+buding983@users.noreply.github.com>
2023-04-25 21:41:20 +0800

    修改主线13图寻敌逻辑
    空弹前优先打大型，空弹后优先级调整为：大型侦察>中型>小型>大型其他

--- campaign/campaign_main/campaign_13_4.py
+++ campaign/campaign_main/campaign_13_4.py
@@ -75,7 +75,7 @@ class Campaign(CampaignBase):
     MAP = MAP
 
     def battle_0(self):
-        if self.clear_filter_enemy('1L > 1M > 2L > 2M > 3L > 2E > 3E > 2C > 3C > 3M', preserve=0):
+        if self.clear_filter_enemy('3E > 3L > 3M > 3C > 2E > 2L > 2M > 2C > 1L > 1M', preserve=0):
             return True
 
         return self.battle_default()
@@ -83,7 +83,7 @@ class Campaign(CampaignBase):
     def battle_3(self):
         self.pick_up_ammo()
 
-        if self.clear_filter_enemy('1L > 1M > 2L > 2M > 3L > 2E > 3E > 2C > 3C > 3M', preserve=0):
+        if self.clear_filter_enemy('3L > 2E > 2L > 2M > 2C > 1L > 1M > 3E > 3C > 3M', preserve=0):
             return True
 
         return self.battle_default()
